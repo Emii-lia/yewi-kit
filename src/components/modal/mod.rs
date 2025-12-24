b3cecc379c4fb8a6be004cdd545a3e3ccac4fee8
Emii-lia <fiaromiangaly@gmail.com>
2025-12-24 17:13:00 +0300

    feat(modal): integrating modal component

---
+++ src/components/modal/mod.rs
@@ -0,0 +1,57 @@
+mod use_modal;
+mod provider;
+mod use_modal_store;
+
+use web_sys::MouseEvent;
+use yew::{classes, function_component, html, Callback, Html};
+use crate::components::Button;
+use crate::types::{ButtonVariant, Size};
+pub use use_modal::*;
+pub use provider::*;
+pub use use_modal_store::*;
+
+#[function_component(Modal)]
+pub(crate) fn modal() -> Html {
+  let modal_vars = use_modal_store();
+  let ModalHookResponse {
+    on_close,
+    classes,
+    is_open,
+    children,
+    ..
+  } = modal_vars.clone();
+  
+  html! {
+    if is_open.clone() {
+      <div
+        class={classes!("Modal")}
+        onclick={{
+          let on_close = on_close.clone();
+          move |_| { on_close.emit(()); }
+        }}
+      >
+        <div
+          class={classes!("modal-body", classes)}
+          onclick={|e: MouseEvent| { e.stop_propagation();}}
+        >
+          <div class="modal-header">
+            <Button
+              variant={ButtonVariant::Tertiary}
+              size={Size::Small}
+              onclick={{
+                let on_close = on_close.clone();
+                Callback::from(move |e: MouseEvent| {
+                  e.stop_propagation();
+                  on_close.emit(());
+                })
+              }}
+            >
+              {"x"}
+            </Button>
+          </div>
+          {modal_vars.children.clone()}
+        </div>
+      </div>
+    }
+  }
+}
