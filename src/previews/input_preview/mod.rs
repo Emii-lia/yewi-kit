22cc1aafa33bfe87ea164471420895d0347333ee
Emii-lia <fiaromiangaly@gmail.com>
2025-12-25 17:23:46 +0300

    feat(input): adding error preview for input preview

--- src/previews/input_preview/mod.rs
+++ src/previews/input_preview/mod.rs
@@ -5,6 +5,9 @@ use crate::types::Size;
 
 #[function_component(InputPreview)]
 pub(crate) fn input_preview() -> Html {
+  let mut errors: Vec<String> = Vec::new();
+  errors.push("Invalid input".to_string());
+
   html! {
     <div class="PreviewSection">
       <h1 class="preview-title">{"Input"}</h1>
@@ -18,6 +21,11 @@ pub(crate) fn input_preview() -> Html {
           <Input placeholder={"Cannot type here..."} input_size={Size::Small} disabled=true/>
           <Input placeholder={"Cannot type here..."} input_size={Size::Medium} disabled=true/>
           <Input placeholder={"Cannot type here..."} input_size={Size::Large} disabled=true/>
+        </PreviewContainer>
+        <PreviewContainer title={"With error"}>
+          <Input placeholder={"Type here..."} input_size={Size::Small} errors={errors.clone()}/>
+          <Input placeholder={"Type here..."} input_size={Size::Medium} errors={errors.clone()}/>
+          <Input placeholder={"Type here..."} input_size={Size::Large} errors={errors.clone()}/>
         </PreviewContainer>
       </div>
     </div>
