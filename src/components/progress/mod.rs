412e46c89117897b911aaaf78468bc274d7fbe66
Emii-lia <fiaromiangaly@gmail.com>
2026-01-05 18:45:10 +0300

    feat(progress): integrating progress component

---
+++ src/components/progress/mod.rs
@@ -0,0 +1,49 @@
+use yew::{classes, function_component, html, Classes, Html, Properties};
+use crate::types::Color;
+
+#[derive(Properties, PartialEq, Clone)]
+pub struct Props {
+  #[prop_or_default]
+  pub value: u32,
+  #[prop_or(100)]
+  pub max: u32,
+  #[prop_or_default]
+  pub class: Classes,
+  #[prop_or(10)]
+  pub height: u32,
+  #[prop_or(Color::Blue)]
+  pub color: Color,
+  #[prop_or_default]
+  pub show_percentage: bool,
+}
+#[function_component(Progress)]
+pub(crate) fn progress(props: &Props) -> Html {
+  let color_class = format!("{:?}", props.color).to_lowercase();
+  let progress_percentage = if props.max == 0 { 0.0 } else { (props.value.min(props.max) as f64 / props.max as f64) * 100.0 };
+
+  html! {
+    <div class="Progress">
+      <div
+        class={classes!(
+          "progress-bar",
+          color_class,
+          &props.class
+        )}
+        style={format!("--value: {}%; height: {}px;",progress_percentage, props.height)}
+      />
+      {
+        html! {
+          if props.show_percentage {
+            <span class="progress-value">
+              {format!(
+                "{}%",
+                progress_percentage.round()
+              )}
+            </span>
+          }
+        }
+      }
+    </div>
+  }
+
+}
