425445f86396b69543029040cab383a5e5b721dd
Emii-lia <fiaromiangaly@gmail.com>
2026-01-07 18:36:49 +0300

    feat(collapse): adding collapse toggle variant preview

--- src/previews/collapse_preview/mod.rs
+++ src/previews/collapse_preview/mod.rs
@@ -1,5 +1,5 @@
 use yew::{function_component, html, Html};
-use crate::components::Collapse;
+use crate::components::{Collapse, CollapseVariant};
 use crate::previews::PreviewContainer;
 
 #[function_component(CollapsePreview)]
@@ -8,8 +8,16 @@ pub(crate) fn collapse_preview() -> Html {
     <div class="PreviewSection">
       <h1 class="preview-title">{"Collapse"}</h1>
       <div class="preview-list">
-        <PreviewContainer title={"Default"}>
+        <PreviewContainer title={"Focus Variant"}>
           <Collapse summary={"See more details"}>
+            <div class="ex-collapse-content">
+              <h2 class="ex-collapse-title">{ "This is a Collapse" }</h2>
+              <p class="ex-collapse-description">{ "Collapse toggles its content vibisi- vilisibili- vibilisi-... visibilility" }</p>
+            </div>
+          </Collapse>
+        </PreviewContainer>
+        <PreviewContainer title={"Toggle Variant"}>
+          <Collapse summary={"See more details"} variant={CollapseVariant::Toggle}>
             <div class="ex-collapse-content">
               <h2 class="ex-collapse-title">{ "This is a Collapse" }</h2>
               <p class="ex-collapse-description">{ "Collapse toggles its content vibisi- vilisibili- vibilisi-... visibilility" }</p>
