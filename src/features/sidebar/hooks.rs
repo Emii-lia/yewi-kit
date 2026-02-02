43ff8e71238f18542030d0cc4972c4b847754ddb
Emii-lia <fiaromiangaly@gmail.com>
2026-02-02 18:31:11 +0300

    feat(sidebar): grouping sidebar menu items

---
+++ src/features/sidebar/hooks.rs
@@ -0,0 +1,37 @@
+use yew::hook;
+use crate::app::DocsRoute;
+
+#[derive(Clone)]
+pub struct RouteSection {
+  pub title: String,
+  pub routes: Vec<DocsRoute>
+}
+#[hook]
+pub fn use_sidebar() -> Vec<RouteSection> {
+  let routes: Vec<DocsRoute> = DocsRoute::iter()
+    .filter(|route| *route != DocsRoute::Docs)
+    .map(|route| route.clone())
+    .collect();
+
+  let mut route_sections: Vec<RouteSection> = Vec::new();
+  let mut get_started: Vec<DocsRoute> = Vec::new();
+  let mut components: Vec<DocsRoute> = Vec::new();
+
+  for route in routes {
+    match route {
+      DocsRoute::Installation => get_started.push(route),
+      _ => components.push(route)
+    }
+  }
+
+  route_sections.push(RouteSection {
+    title: "Getting Started".to_string(),
+    routes: get_started
+  });
+  route_sections.push(RouteSection {
+    title: "Components".to_string(),
+    routes: components
+  });
+
+  route_sections
+}
--- src/features/sidebar/mod.rs
+++ src/features/sidebar/mod.rs
@@ -1,54 +1,49 @@
+mod hooks;
+
 mod components {
   pub mod nav_item;
 }
 
-use yew::{function_component, html, Html};
+use yew::{classes, function_component, html, Html};
+use yew_router::prelude::Link;
 use crate::app::AppRoute;
 use crate::features::sidebar::components::nav_item::NavItem;
+use crate::features::sidebar::hooks::use_sidebar;
 
 #[function_component(Sidebar)]
 pub(crate) fn sidebar() -> Html {
+  let route_group = use_sidebar();
   html! {
-    <div class="Sidebar">
+    <aside class="Sidebar">
       <div class="sidebar-header">
-        <h1 class="sidebar-title">{"Yewi-kit"}</h1>
+        <Link<AppRoute>
+          classes={classes!("sidebar-header-content")}
+          to={AppRoute::Home}
+        >
+          <span class="sidebar-logo">{"Y"}</span>
+          <span class="sidebar-title">{"Yewi-kit"}</span>
+        </Link<AppRoute>>
       </div>
       <div class="sidebar-menu">
-        {
-          AppRoute::iter().filter(|route| *route != AppRoute::NotFound).map(|route| {
-            let label = match route {
-              AppRoute::Home => "Installation",
-              AppRoute::AvatarPage => "Avatar",
-              AppRoute::AvatarGroupPage => "Avatar Group",
-              AppRoute::BadgePage => "Badge",
-              AppRoute::ButtonPage => "Button",
-              AppRoute::CardPage => "Card",
-              AppRoute::CarouselPage => "Carousel",
-              AppRoute::CheckboxPage => "Checkbox",
-              AppRoute::CollapsePage => "Collapse",
-              AppRoute::DividerPage => "Divider",
-              AppRoute::DropdownPage => "Dropdown",
-              AppRoute::FileInput => "File Input",
-              AppRoute::InputPage => "Input",
-              AppRoute::PasswordInputPage => "Password Input",
-              AppRoute::ProgressPage => "Progress",
-              AppRoute::ModalPage => "Modal",
-              AppRoute::RadioPage => "Radio",
-              AppRoute::SelectPage => "Select",
-              AppRoute::TablePage => "Table",
-              AppRoute::TabsPage => "Tabs",
-              AppRoute::TextareaPage => "Textarea",
-              AppRoute::ToastPage => "Toast",
-              AppRoute::NotFound => "Not Found",
-            }.to_string();
-            html! {
-              <NavItem label={label} href={route.clone()}/>
-            }
-          }).collect::<Html>()
-
-        }
-
+        {for route_group.iter().map(|rs| {
+          html! {
+            <div class="sidebar-section">
+              <h3 class="sidebar-section-title">{rs.title.clone()}</h3>
+              <div class="sidebar-section-items">
+                {for rs.routes.iter().map(|route| {
+                  html! {
+                    <NavItem
+                      href={route.clone()}
+                      label={route.clone().to_string()}
+                      key={route.clone().to_string()}
+                    />
+                  }
+                })}
+              </div>
+            </div>
+          }
+        })}
       </div>
-    </div>
+    </aside>
   }
 }
