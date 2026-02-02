e4335182897ce39bb0fc03ab61761f46467024d6
Emii-lia <fiaromiangaly@gmail.com>
2026-02-02 18:31:30 +0300

    refactor(sidebar): using DocsRoute for sidebar nav item

--- src/features/sidebar/components/nav_item/hooks.rs
+++ src/features/sidebar/components/nav_item/hooks.rs
@@ -1,10 +1,10 @@
 use yew::{hook, Properties};
 use yew_router::hooks::use_route;
-use crate::app::AppRoute;
+use crate::app::DocsRoute;
 
 #[derive(PartialEq, Properties, Clone)]
 pub struct HookParams {
-  pub href: AppRoute,
+  pub href: DocsRoute,
 }
 
 #[derive(PartialEq, Clone)]
@@ -14,7 +14,7 @@ pub struct HookResponse {
 
 #[hook]
 pub(crate) fn use_nav_item(params: HookParams) -> HookResponse {
-  let current_route = use_route::<AppRoute>();
+  let current_route = use_route::<DocsRoute>();
   let HookParams { href } = params;
   
   HookResponse { is_active: matches!(current_route, Some(route) if route == href) }
