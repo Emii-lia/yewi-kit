ef7bf525d0090c1221b7c0b6b8160c03410742a0
Emii-lia <fiaromiangaly@gmail.com>
2026-02-03 11:26:20 +0300

    feat(homepage): declaring home_header and hero modules as features and adding them to homepage

--- src/app/page.rs
+++ src/app/page.rs
@@ -1,9 +1,12 @@
 use yew::{function_component, html, Html};
+use crate::features::{Hero, HomeHeader};
 
 #[function_component]
 pub(crate) fn Home() -> Html {
   html! {
-    <div class="Home page-container">
+    <div class="Home">
+      <HomeHeader/>
+      <Hero/>
     </div>
   }
 }
--- src/features/mod.rs
+++ src/features/mod.rs
@@ -1,5 +1,9 @@
 mod sidebar;
 mod installation;
+mod home_header;
+mod hero;
 
 pub(crate) use sidebar::*;
 pub(crate) use installation::*;
+pub(crate) use home_header::*;
+pub(crate) use hero::*;
