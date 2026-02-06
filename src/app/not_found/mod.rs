9f1bd0e34e35df0abb5754b165a99dc701c63aa4
Emii-lia <fiaromiangaly@gmail.com>
2026-02-06 14:31:24 +0300

    refactor(404): updating 404 page style

--- src/app/not_found/mod.rs
+++ src/app/not_found/mod.rs
@@ -3,8 +3,19 @@ use yew::{function_component, html, Html};
 #[function_component(NotFound)]
 pub(crate) fn not_found() -> Html {
   html! {
-    <div class="NotFound">
-      <h1>{"404"}</h1>
-    </div>
+    <main class="NotFound">
+      <div class="not-found-container">
+        <div class="not-found-header">
+          <h1 class="not-found-title">
+            {"404"}
+          </h1>
+          <div class="not-found-line" />
+        </div>
+
+        <h2 class="not-found-subtitle">
+          {"Page Not Found"}
+        </h2>
+      </div>
+    </main>
   }
 }
