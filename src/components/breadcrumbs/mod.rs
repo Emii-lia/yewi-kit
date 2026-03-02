c07ede74cff3738ebda37e07b7471d27c050b5b3
Emii-lia <fiaromiangaly@gmail.com>
2026-03-02 20:23:06 +0300

    feat(breadcrumbs): integrating breadcrumbs component

---
+++ src/components/breadcrumbs/mod.rs
@@ -0,0 +1,45 @@
+use yew::{classes, function_component, html, Classes, Html, Properties};
+use yew::html::IntoPropValue;
+use yew_router::components::Link;
+use yew_router::Routable;
+
+#[derive(Properties, PartialEq ,Clone)]
+pub struct BreadcrumbsProps {
+  pub children: Html,
+  #[prop_or_default]
+  pub class: Classes,
+  #[prop_or("/".into())]
+  pub separator: String
+}
+
+#[function_component(Breadcrumbs)]
+pub fn breadcrumbs(props: &BreadcrumbsProps) -> Html {
+  html! {
+    <div class={classes!("Breadcrumbs", &props.class)} style={format!("--separator: '{}';", props.separator)}>
+      {props.children.clone()}
+    </div>
+  }
+}
+
+#[derive(Properties, PartialEq ,Clone)]
+pub struct BreadCrumbProps<R>
+where
+  R: Routable
+{
+  pub href: R,
+  pub children: Html,
+  #[prop_or_default]
+  pub class: Classes
+}
+
+#[function_component(BreadCrumb)]
+pub fn bread_crumb<R>(props: &BreadCrumbProps<R>) -> Html
+where
+  R: Routable + 'static
+{
+  html! {
+    <Link<R> to={props.href.clone()} classes={classes!("BreadCrumbs__item", &props.class)}>
+      { props.children.clone()}
+    </Link<R>>
+  }
+}
