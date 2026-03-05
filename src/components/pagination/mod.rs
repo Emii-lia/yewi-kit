fa8b18261eb68329d71c4ebb94630189ef7dd67a
Emii-lia <fiaromiangaly@gmail.com>
2026-03-05 19:33:20 +0300

    feat(pagination): integrating pagination component

---
+++ src/components/pagination/mod.rs
@@ -0,0 +1,100 @@
+mod use_pagination;
+pub(crate) mod types;
+
+use yew::{classes, function_component, html, Callback, Classes, Html, Properties};
+use yew_icons::IconData;
+use crate::components::{Button, ButtonVariant};
+use crate::components::pagination::types::PaginationVariant;
+use crate::components::pagination::use_pagination::{use_pagination, PaginationHookParams};
+use crate::types::Size;
+
+#[derive(Properties, PartialEq ,Clone)]
+pub struct Props {
+  pub count: usize,
+  #[prop_or(1)]
+  pub current: usize,
+  #[prop_or_default]
+  pub on_change: Callback<usize>,
+  #[prop_or_default]
+  pub class: Classes,
+  #[prop_or(PaginationVariant::Default)]
+  pub variant: PaginationVariant,
+  #[prop_or(false)]
+  pub rounded: bool,
+  #[prop_or(Size::Medium)]
+  pub size: Size
+}
+
+#[function_component(Pagination)]
+pub fn pagination(props: &Props) -> Html {
+  let (
+    current_page,
+    go_to_page,
+    page_numbers
+  ) = use_pagination(PaginationHookParams {
+    current: props.current,
+    on_change: props.on_change.clone(),
+    count: props.count
+  });
+
+  let btn_variant: ButtonVariant = match props.variant {
+    PaginationVariant::Default => ButtonVariant::Tertiary,
+    PaginationVariant::Outlined => ButtonVariant::Secondary
+  };
+
+  html! {
+    <div class={classes!("Pagination", &props.class)}>
+      <Button
+        icon={IconData::LUCIDE_CHEVRON_LEFT}
+        variant={btn_variant.clone()}
+        size={props.size.clone()}
+        rounded={props.rounded}
+        disabled={current_page == 1}
+        onclick={{
+          let go_to_page = go_to_page.clone();
+          Callback::from(move |_| {
+            go_to_page.emit(current_page.clone() - 1);
+          })}
+        }
+      />
+      <div class="Pagination__numbers">
+        {for page_numbers.iter().map(|number| {
+          if number == "..." {
+            html! { <span class="Pagination__ellipsis">{number}</span> }
+          } else {
+            let page_num = number.parse::<usize>().unwrap_or(1);
+            html! {
+              <Button
+                key={number.clone()}
+                variant={if page_num == current_page { ButtonVariant::Primary } else { btn_variant.clone() }}
+                size={props.size.clone()}
+                rounded={props.rounded}
+                onclick={{
+                  let go_to_page = go_to_page.clone();
+                  Callback::from(move |_| {
+                    go_to_page.emit(page_num);
+                  })}
+                }
+              >
+                {number}
+              </Button>
+            }
+          }
+        })}
+      </div>
+      <Button
+        icon={IconData::LUCIDE_CHEVRON_RIGHT}
+        variant={btn_variant.clone()}
+        size={props.size.clone()}
+        rounded={props.rounded}
+        disabled={current_page == props.count}
+        onclick={{
+          let go_to_page = go_to_page.clone();
+          Callback::from(move |_| {
+            go_to_page.emit(current_page.clone() + 1);
+          })}
+        }
+      />
+    </div>
+  }
+}
---
+++ src/components/pagination/use_pagination.rs
@@ -0,0 +1,58 @@
+use web_sys::MouseEvent;
+use yew::{hook, use_state, Callback};
+
+#[derive(Clone)]
+pub(crate) struct PaginationHookParams {
+  pub(crate) count: usize,
+  pub(crate) current: usize,
+  pub(crate) on_change: Callback<usize>
+}
+
+fn get_page_numbers(count: usize, current: usize) -> Vec<String> {
+  let mut page_numbers = Vec::new();
+  if count <= 5 {
+    for i in 1..=count {
+      page_numbers.push(i.to_string());
+    }
+  } else {
+    if current > 3 {
+      page_numbers.push("1".to_string());
+      if current > 4 {
+        page_numbers.push("...".to_string());
+      }
+    }
+    let start = if current > 2 { current - 2 } else { 1 };
+    let end = if current < count - 2 { current + 2 } else { count };
+    for i in start..=end {
+      page_numbers.push(i.to_string());
+    }
+    if current < count - 2 {
+      if current < count - 3 {
+        page_numbers.push("...".to_string());
+      }
+      page_numbers.push(count.to_string());
+    }
+  }
+  page_numbers
+}
+
+#[hook]
+pub(crate) fn use_pagination (params: PaginationHookParams)
+ -> ( usize, Callback<usize>, Vec<String> ) {
+  let PaginationHookParams { count, current, on_change } = params.clone();
+  let current_page = use_state(|| current);
+
+  let go_to_page = {
+    let current_page = current_page.clone();
+    Callback::from(move |page: usize| {
+      if (page > 0 && page <= count) {
+        current_page.set(page);
+        on_change.emit(page);
+      }
+    })
+  };
+
+  let page_numbers = get_page_numbers(count, *current_page);
+
+  (*current_page, go_to_page, page_numbers)
+}
