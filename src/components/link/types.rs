63c4079cb92f738a8fe73ae6e6d3ac5d77b7ad87
Emii-lia <fiaromiangaly@gmail.com>
2026-03-19 15:35:54 +0300

    feat(link): integrating link component

---
+++ src/components/link/types.rs
@@ -0,0 +1,16 @@
+use std::fmt::Display;
+
+#[derive(PartialEq, Clone)]
+pub enum LinkVariant {
+  Default,
+  Button
+}
+
+impl LinkVariant {
+  pub fn as_str(&self) -> &'static str {
+    match self {
+      LinkVariant::Default => "default",
+      LinkVariant::Button => "button"
+    }
+  }
+}
