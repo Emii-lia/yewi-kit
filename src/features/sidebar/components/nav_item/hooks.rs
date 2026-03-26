c493030f2be65634b2cdb30336aa04510a975f8a
Emii-lia <fiaromiangaly@gmail.com>
2026-03-26 15:31:07 +0300

    refactor(sidebar): using anchor instead of navigator hook

--- src/features/sidebar/components/nav_item/hooks.rs
+++ src/features/sidebar/components/nav_item/hooks.rs
@@ -1,7 +1,6 @@
 use web_sys::MouseEvent;
 use yew::{hook, Callback, Properties};
 use yew_router::hooks::use_route;
-use yew_router::prelude::use_navigator;
 use crate::app::docs::routes::DocsRoute;
 use crate::features::provider::SidebarContext;
 use crate::features::sidebar::store::use_sidebar_store;
@@ -20,15 +19,12 @@ pub struct HookResponse {
 #[hook]
 pub(crate) fn use_nav_item(params: HookParams) -> HookResponse {
   let current_route = use_route::<DocsRoute>();
-  let navigator = use_navigator().unwrap();
   let SidebarContext { toggle_open ,.. } = use_sidebar_store();
   let HookParams { href } = params;
 
   let go = {
-    let href = href.clone();
     Callback::from(move |e: MouseEvent| {
       toggle_open.emit(e);
-      navigator.push(&href);
     })
   };
   HookResponse { 
