3f3156d03539cf3d47262a5332076748d4b74b51
Emii-lia <fiaromiangaly@gmail.com>
2026-04-13 15:49:43 +0300

    feat(collapse): adding indicator prop change between Plus and Chevron for collapse

--- src/components/collapse/types.rs
+++ src/components/collapse/types.rs
@@ -3,3 +3,18 @@ pub(crate) enum CollapseVariant {
   Toggle,
   Focus
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) enum CollapseIndicator {
+  Plus,
+  Chevron
+}
+
+impl CollapseIndicator {
+  pub fn as_str(&self) -> &'static str {
+    match self {
+      CollapseIndicator::Plus => "plus",
+      CollapseIndicator::Chevron => "chevron",
+    }
+  }
+}
