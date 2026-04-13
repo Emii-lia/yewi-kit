8bf0202fa875c3e92173b5f0f18484930b51e6ef
Emii-lia <fiaromiangaly@gmail.com>
2026-04-13 15:53:36 +0300

    feat(sidebar): integrating sidebar sub menu components

--- src/components/sidebar/mod.rs
+++ src/components/sidebar/mod.rs
@@ -4,6 +4,7 @@ use yew_icons::IconData;
 use crate::components::sidebar::provider::SidebarContextType;
 use store::use_sidebar_store;
 use crate::components::button::{Button, ButtonVariant};
+use crate::components::collapse::{Collapse, CollapseContent, CollapseIndicator, CollapseTrigger, CollapseVariant};
 use crate::components::sidebar::types::{SidebarConfig, SidebarPosition};
 use crate::types::Size;
 
@@ -27,6 +28,7 @@ pub fn sidebar(props: &SidebarProps) -> Html {
     state,
     open_mobile,
     set_open_mobile,
+    sidebar_ref,
     ..
   } = use_sidebar_store();
   let SidebarConfig {
@@ -37,20 +39,35 @@ pub fn sidebar(props: &SidebarProps) -> Html {
 
   if is_mobile {
     html! {
-      <div
-        class={classes!("Sidebar", &props.class)}
-        data-open-mobile={open_mobile.to_string()}
-        data-position={props.position.clone().as_str()}
-        data-mobile={"true"}
-        style={format!("--sidebar-width: {};", width_mobile)}
-      >
-        {props.children.clone()}
-      </div>
+      <>
+        <div
+          class={classes!("Sidebar", &props.class)}
+          data-open-mobile={open_mobile.to_string()}
+          data-position={props.position.clone().as_str()}
+          data-mobile={"true"}
+          ref={sidebar_ref.clone()}
+          style={format!("--sidebar-width: {};", width_mobile)}
+        >
+          {props.children.clone()}
+        </div>
+        <div
+          class={"sidebar-overlay"}
+          onclick={{
+            let set_open_mobile = set_open_mobile.clone();
+            Callback::from(move |_| {
+              if open_mobile {
+                set_open_mobile.emit(false);
+              }
+            })
+          }}
+        />
+      </>
     }
   } else {
     html! {
       <div
         class={classes!("Sidebar", &props.class)}
+        data-el="sidebar"
         data-state={state.clone().as_str()}
         data-position={props.position.clone().as_str()}
         data-mobile={"false"}
@@ -89,10 +106,12 @@ pub fn sidebar_trigger(props: &SidebarTriggerProps) -> Html {
     <Button
       size={props.size.clone()}
       icon={props.icon.clone()}
+      id={"sidebar-trigger"}
       onclick={{
         let toggle_sidebar = toggle_sidebar.clone();
         let onclick = props.onclick.clone();
-        Callback::from(move |_| {
+        Callback::from(move |e: MouseEvent| {
+          e.stop_propagation();
           toggle_sidebar.emit(());
           onclick.emit(());
         })
@@ -217,5 +236,38 @@ pub fn sidebar_menu_item(props: &SidebarMenuItemProps) -> Html {
         {props.action.clone()}
       </div>
     </div>
+  }
+}
+
+#[component(SidebarSubMenu)]
+pub fn sidebar_sub_menu(props: &SidebarChildrenWithClassProps) -> Html {
+  html! {
+    <Collapse
+      variant={CollapseVariant::Toggle}
+      class={classes!("SidebarSubMenu", &props.class)}
+    >
+      {props.children.clone()}
+    </Collapse>
+  }
+}
+
+#[component(SidebarSubMenuTitle)]
+pub fn sidebar_sub_menu_title(props: &SidebarChildrenWithClassProps) -> Html {
+  html! {
+    <CollapseTrigger
+      class={classes!("SidebarSubMenuTitle", &props.class)}
+      indicator={CollapseIndicator::Chevron}
+    >
+      {props.children.clone()}
+    </CollapseTrigger>
+  }
+}
+
+#[component(SidebarSubMenuContent)]
+pub fn sidebar_sub_menu_content(props: &SidebarChildrenWithClassProps) -> Html {
+  html! {
+    <CollapseContent class={classes!("SidebarSubMenuContent", &props.class)}>
+      {props.children.clone()}
+    </CollapseContent>
   }
 }
