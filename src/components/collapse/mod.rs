b35dc7bfef78858731a943e6df8a25a3ba754feb
Emii-lia <fiaromiangaly@gmail.com>
2026-04-13 15:50:39 +0300

    feat(collapse): optimizing collapse component structure by adding trigger and content components and adding icon prop for trigger component

--- src/components/collapse/mod.rs
+++ src/components/collapse/mod.rs
@@ -2,28 +2,73 @@ mod types;
 mod hooks;
 
 use yew::{classes, component, html, AttrValue, Callback, Classes, Html, Properties};
+use yew_icons::{Icon, IconData};
 pub(crate) use types::*;
 use crate::components::collapse::hooks::{use_collapse, HookResponse};
 
 #[derive(Properties, Clone, PartialEq)]
-pub struct Props {
-  #[prop_or_default]
-  pub summary: AttrValue,
+pub struct CollapseProps {
   pub children: Html,
   #[prop_or_default]
   pub class: Classes,
   #[prop_or(CollapseVariant::Focus)]
   pub variant: CollapseVariant,
 }
+
+#[derive(Properties, Clone, PartialEq)]
+pub struct CollapseTriggerProps {
+  pub children: Html,
+  #[prop_or_default]
+  pub class: Classes,
+  #[prop_or_default]
+  pub icon: Option<IconData>,
+  #[prop_or(CollapseIndicator::Plus)]
+  pub indicator: CollapseIndicator
+}
+
+#[derive(Properties, Clone, PartialEq)]
+pub struct CollapseContentProps {
+  pub children: Html,
+  #[prop_or_default]
+  pub class: Classes,
+}
+#[component(CollapseTrigger)]
+pub(crate) fn collapse_trigger(props: &CollapseTriggerProps) -> Html {
+  html! {
+    <div
+      class={classes!("CollapseTrigger", &props.class)}
+      data-indicator={props.indicator.as_str()}
+    >
+      <div class={"collapse-trigger-content"}>
+        {html! {
+          if let Some(icon) = props.icon {
+            <Icon data={icon} class="collapse-trigger-icon"/>
+          }
+        }}
+        {props.children.clone()}
+      </div>
+    </div>
+  }
+}
+
+#[component(CollapseContent)]
+pub(crate) fn collapse_content(props: &CollapseContentProps) -> Html {
+  html! {
+    <div class={classes!("CollapseContent", &props.class)}>
+      {props.children.clone()}
+    </div>
+  }
+}
+
 #[component(Collapse)]
-pub(crate) fn collapse(props: &Props) -> Html {
+pub(crate) fn collapse(props: &CollapseProps) -> Html {
   let HookResponse { is_open, toggle,checkbox_ref } = use_collapse();
 
   match props.variant {
     CollapseVariant::Toggle => {
       html! {
         <div
-          class={classes!("Collapse__toggle", is_open.then_some("open"))}
+          class={classes!("Collapse", "toggle",is_open.then_some("open"), &props.class)}
           onclick={{
             let toggle = toggle.clone();
             Callback::from(move |_e| {
@@ -43,20 +88,14 @@ pub(crate) fn collapse(props: &Props) -> Html {
             checked={is_open}
             ref={checkbox_ref.clone()}
           />
-          <div class="collapse-trigger">{&props.summary}</div>
-          <div class={classes!("collapse-content", &props.class)}>
-            {props.children.clone()}
-          </div>
+          {props.children.clone()}
         </div>
       }
     },
     CollapseVariant::Focus => {
       html! {
-        <div class="Collapse" tabindex={"1"}>
-          <div class="collapse-trigger">{&props.summary}</div>
-          <div class={classes!("collapse-content", &props.class)}>
-            {props.children.clone()}
-          </div>
+        <div class={classes!("Collapse", &props.class)} tabindex={"1"}>
+          {props.children.clone()}
         </div>
       }
     }
